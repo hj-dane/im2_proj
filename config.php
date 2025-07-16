51eb3e0f46c46274f275a33a9ff44ee67619204e
hj-dane <160499296+hj-dane@users.noreply.github.com>
2025-07-16 09:11:55 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
-    $host = 'c1-link.com';
-    $user = 'c1link_aishen';
-    $pass = 'SEULRENE_kangseulgi';
-    $db = 'c1link_usc_db';
+$host = 'localhost';
+$dbname = 'school_db';
+$username = 'root';
+$password = '';
 
-    $mysqli = new mysqli($host, $user, $pass, $db);
+$mysqli = new mysqli($host, $username, $password, $dbname);
 
-    // Check connection
-    if ($mysqli->connect_error) {
-        die("Database connection failed: " . $mysqli->connect_error);
-    }
+// Check connection
+if ($mysqli->connect_error) {
+    die("Database connection failed: " . $mysqli->connect_error);
+}
 
-    // Optional: Set charset to UTF-8
-    $mysqli->set_charset("utf8mb4");
+// Optional: Set charset to UTF-8
+$mysqli->set_charset("utf8mb4");
 ?>
